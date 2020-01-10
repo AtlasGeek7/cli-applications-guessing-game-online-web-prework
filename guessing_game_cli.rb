7995e4a4ca4f2666590a8b2f35c212be88d3da64
Khalid Tissire <ghettoguy7@gmail.com>
2020-01-10 04:56:39 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,20 @@ def run_guessing_game
   
 end
 
+def gen_rand
+  rand(6) + 1
+end
+
 def prompt_user
   puts "Guess the number? (1-6)"
 end
 
-def gen_rand
-  rand(6) + 1
+def get_user_input
+  gets.chomp.strip
 end
+
+def process_input
+  
+end
+
+
