1cb0427c37fd4d0afd3b96ac8d003e23bf8f4ab5
Khalid Tissire <ghettoguy7@gmail.com>
2020-01-10 05:03:59 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -18,18 +18,14 @@ def get_user_input
 end
 
 def process_input(randNum,input)
-  case capacity
-  when 0
-    "You ran out of gas."
-  when 1..20
+  case input
+  when randNum
+    puts "You ran out of gas."
+  when "exit"
     "The tank is almost empty. Quickly, find a gas station!"
-  when 21..70
-  "You should be ok for now."
-when 71..100
-  "The tank is almost full."
-else
+  else
   "Error: capacity has an invalid value (#{capacity})"
-end
+  end
 end
 
 
